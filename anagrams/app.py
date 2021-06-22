43ca01ec895fc942dc5b1cba16784be3bdec4caf
Kristaspahilari <66671551+Kristaspahilari@users.noreply.github.com>
2021-06-22 21:22:23 +0200

    Add files via upload

---
+++ anagrams/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route("/wordss")
+def wordss():
+    f = open('words.txt')
+    wordss = f.read().splitlines()
+    return render_template('words.html', word_list=wordss)
+
+@app.route("/words/<word>")
+def words(word):
+    f = open('words.txt')
+    wordss = f.read().splitlines()
+
+    words = []
+    
+    for w in wordss:
+        if sorted(w) == sorted(word):
+            words.append(w)
+               
+    return render_template('words.html', word_list=words)
+
